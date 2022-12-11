ab08df162a298fde87b0e8b7058fecebe55c7425
louiscklaw <louiscklaw@users.noreply.github.com>
2022-12-11 20:07:56 +0800

    update sherlock,

---
+++ test.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+# python3 sherlock louiscklaw
+# # python3 sherlock louiscklaw --print-all
+
+# python3 sherlock le_sister
+# # python3 sherlock le_sister --print-all
+
+# python3 sherlock james_endl
+# # python3 sherlock james_endl --print-all
+
+# python3 sherlock man1130
+# # python3 sherlock man1130 --print-all
+
+python3 sherlock Man1130
+# python3 sherlock Man1130 --print-all
+
+python3 sherlock stephanie1104
+# python3 sherlock stephanie1104 --print-all
+
+python3 sherlock payyyyyy
+# python3 sherlock payyyyyy --print-all
