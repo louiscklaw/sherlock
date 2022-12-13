0b129d99d4fd0902122e1c02061d358238013eef
louiscklaw <louiscklaw@users.noreply.github.com>
2022-12-13 11:58:54 +0800

    update sherlock,

--- test.sh
+++ test.sh
@@ -1,37 +1,18 @@
 #!/usr/bin/env bash
 
-# python3 sherlock louiscklaw
-# # python3 sherlock louiscklaw --print-all
-
-# python3 sherlock le_sister
-# # python3 sherlock le_sister --print-all
-
-# python3 sherlock james_endl
-# # python3 sherlock james_endl --print-all
-
-# python3 sherlock man1130
-# # python3 sherlock man1130 --print-all
-
-# python3 sherlock Man1130
-# python3 sherlock Man1130 --print-all
-
-# python3 sherlock stephanie1104
-# python3 sherlock stephanie1104 --print-all
-
-# python3 sherlock payyyyyy
-# python3 sherlock payyyyyy --print-all
-
-# python3 sherlock yeung_sonson
-# python3 sherlock yeung_sonson --print-all
-
-# python3 sherlock jasonphf
-# # python3 sherlock jasonphf --print-all
-
-# python3 sherlock yijiachin1
-# # python3 sherlock yijiachin1 --print-all
-
-
 rm -rf sherlock/__pycache__
 
-python3 sherlock wongraymondb --output ./dist/wongraymondb.html
-# python3 sherlock wongraymondb --print-all
+python3 sherlock louiscklaw --output ./dist/louiscklaw.html &
+python3 sherlock le_sister --output ./dist/le_sister.html &
+python3 sherlock james_endl --output ./dist/james_endl.html &
+python3 sherlock man1130 --output ./dist/man1130.html &
+python3 sherlock Man1130 --output ./dist/Man1130.html &
+python3 sherlock stephanie1104 --output ./dist/stephanie1104.html &
+python3 sherlock payyyyyy --output ./dist/payyyyyy.html &
+python3 sherlock yeung_sonson --output ./dist/yeung_sonson.html &
+python3 sherlock jasonphf --output ./dist/jasonphf.html &
+python3 sherlock yijiachin1 --output ./dist/yijiachin1.html &
+python3 sherlock wongraymondb --output ./dist/wongraymondb.html &
+python3 sherlock kylema11201 --output ./dist/kylema11201.html &
+
+wait
